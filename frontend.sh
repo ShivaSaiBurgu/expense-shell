bab904cdd60dd9b37291178be9ef0928145b8025
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-05 19:34:54 +0530

    Added backend.sh

---
+++ frontend.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+USER=$(id -u)
+if [ $USER -ne 0 ]
+then
+echo "please run the script with root access"
+exit
+else
+echo "You are a root user"
+fi
+time=$(date +%F-%H-%M-%S)
+script=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$time-$script.log
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo "$2...Failure"
+    exit
+    else
+    echo "$2...success"
+    fi
+
+}
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing existing content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html &>>$LOGFILE
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting frontend code"
+
+#check your repo and path
+cp /home/ec2-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copied expense conf"
+
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting nginx"
