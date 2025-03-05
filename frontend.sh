3ea46b6ec98aeae1b5fcf151e8079f39afb86a38
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-05 19:36:31 +0530

    Added backend.sh

--- frontend.sh
+++ frontend.sh
@@ -41,7 +41,7 @@ unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
 #check your repo and path
-cp /home/ec2-user/shell-script/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
 systemctl restart nginx &>>$LOGFILE
