53c769bab7427ce139e7a7966d322c19d3bf3653
Burgu Shiva Sai <burgushivasai114@gmail.com>
2025-03-05 18:54:24 +0530

    Added backend.sh

---
+++ backend.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+USER=$(id -u)
+if [ $USER -ne 0 ]
+then
+echo "please run the script with root access"
+exit
+else
+echo "You are a root user"
+fi
+echo "please enter db password"
+read password
+time=$(date +%F-%H-%M-%S)
+script=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$time-$script.log
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+    echo "$2...Failure"
+    exit
+    else
+    echo "$2...success"
+    fi
+
+}
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs:20 version"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+id expense
+if [ $? -eq 0 ]
+then
+echo "expense user already added"
+else
+useradd expense &>>LOGFILE
+validate $? "Added expense user"
+fi
